1a42ff22dcffbad4031ba4f7fbef52b31596a31a
BazarnovSergey <ssergey101@yandex.ru>
2022-06-25 17:25:19 +0300

    4. Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,21 +1,62 @@
 package ru.job4j.concurrent;
 
-public class Wget {
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
 
-    public static void main(String[] args) {
-        Thread thread = new Thread(
-                () -> {
-                    try {
-                        for (int index = 0; index <= 100; index++) {
-                            Thread.sleep(100);
-                            System.out.print("\rLoading : " + index + "%");
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-        );
-        thread.start();
+public class Wget implements Runnable {
+
+    public final static long MILLISECONDS = 1000;
+    private final String url;
+    private final int speed;
+    private final String fileName;
+
+    public Wget(String url, int speed, String fileName) {
+        this.url = url;
+        this.speed = speed;
+        this.fileName = fileName;
     }
 
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            long totalBytes = 0;
+            long start = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                totalBytes += bytesRead;
+                if (totalBytes >= speed) {
+                    long time = System.currentTimeMillis() - start;
+                    if (time <= 1000) {
+                        Thread.sleep(MILLISECONDS - time);
+                    }
+                    totalBytes = 0;
+                    start = System.currentTimeMillis();
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void checkValidateArgs(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Check the correctness of the arguments."
+                    + "\nEnter 3 parameters: url, speed and file name to save");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        checkValidateArgs(args);
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        String fileName = args[2];
+        Thread wget = new Thread(new Wget(url, speed, fileName));
+        wget.start();
+        wget.join();
+    }
 }
