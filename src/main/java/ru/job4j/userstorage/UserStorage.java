6fb614ae34b3dc06236153e16fd8960f1a3fb571
BazarnovSergey <ssergey101@yandex.ru>
2022-06-30 19:51:33 +0300

    3. Класс хранилища пользователей UserStorage [#1104 #317357]

--- src/main/java/ru/job4j/userstorage/UserStorage.java
+++ src/main/java/ru/job4j/userstorage/UserStorage.java
@@ -3,56 +3,35 @@ package ru.job4j.userstorage;
 import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 @ThreadSafe
 public class UserStorage implements Storage {
 
     @GuardedBy("this")
-    private final List<User> userList;
-
-    public UserStorage(List<User> users) {
-        synchronized (UserStorage.class) {
-            this.userList = new ArrayList<>(users);
-        }
-    }
+    private final ConcurrentHashMap<Integer, User> userList = new ConcurrentHashMap<>();
 
     @Override
     public synchronized boolean add(User user) {
-        boolean rsl = false;
-        if (!userList.contains(user)) {
-            userList.add(new User(user.getId(), user.getAmount()));
-            rsl = true;
-        }
-        return rsl;
+        return userList.putIfAbsent(user.getId(), user) != null;
     }
 
     @Override
     public synchronized boolean update(User user) {
-        boolean rsl = false;
-        if (userList.get(user.getId() - 1).getId() == user.getId()) {
-            userList.set(user.getId(), new User(user.getId(), user.getAmount()));
-            rsl = true;
-        }
-        return rsl;
+        return userList.replace(user.getId(), user) != null;
     }
 
     @Override
     public synchronized boolean delete(User user) {
-        return userList.remove(user);
+        return userList.remove(user.getId()) != null;
     }
 
     public synchronized void transfer(int fromId, int toId, int amount) {
-        User userFrom = this.userList.stream()
-                .filter(userFirst -> userFirst.getId() == fromId)
-                .findFirst().orElse(null);
-        User userTo = this.userList.stream()
-                .filter(userSecond -> userSecond.getId() == toId)
-                .findFirst().orElse(null);
+        User userFrom = userList.get(fromId);
+        User userTo = userList.get(toId);
         if (userFrom != null && userFrom.getAmount() >= amount) {
-            userFrom.setAmount(userFrom.getAmount() - amount);
-            userTo.setAmount(userTo.getAmount() + amount);
+            userList.get(fromId).setAmount(userFrom.getAmount() - amount);
+            userList.get(toId).setAmount(userTo.getAmount() + amount);
         }
     }
 }
--- src/test/java/ru/job4j/userstorage/UserStorageTest.java
+++ src/test/java/ru/job4j/userstorage/UserStorageTest.java
@@ -2,37 +2,30 @@ package ru.job4j.userstorage;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class UserStorageTest {
 
-    private static class ThreadTransfer extends Thread {
-        private final UserStorage userStorage;
-
-        private ThreadTransfer(UserStorage userStorage) {
-            this.userStorage = userStorage;
-        }
-
-        @Override
-        public void run() {
-            this.userStorage.transfer(1, 2, 10);
-        }
-    }
-
     @Test
-    public void whenThread1TransferUser1ToUser2() throws InterruptedException {
-        List<User> userList = new ArrayList<>();
+    public void whenTwoThreadsTransferUser1ToUser2() throws InterruptedException {
+
+        UserStorage userStorage = new UserStorage();
         User user1 = new User(1, 100);
         User user2 = new User(2, 200);
-        userList.add(user1);
-        userList.add(user2);
-        UserStorage userStorage = new UserStorage(userList);
-        Thread thread1 = new ThreadTransfer(userStorage);
-        Thread thread2 = new ThreadTransfer(userStorage);
+        userStorage.add(user1);
+        userStorage.add(user2);
+
+        Thread thread1 = new Thread(
+                () -> {
+                    userStorage.transfer(1, 2, 10);
+                }
+        );
+        Thread thread2 = new Thread(
+                () -> {
+                    userStorage.transfer(1, 2, 10);
+                }
+        );
         thread1.start();
         thread1.join();
         thread2.start();
