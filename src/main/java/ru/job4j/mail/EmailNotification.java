4350493c687938bc94e1b0180e646ef23a63397e
BazarnovSergey <ssergey101@yandex.ru>
2022-07-06 23:34:39 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/mail/EmailNotification.java
@@ -0,0 +1,52 @@
+package ru.job4j.mail;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+
+    private final ExecutorService pool;
+
+    public EmailNotification() {
+        this.pool = Executors.newFixedThreadPool(
+                Runtime.getRuntime().availableProcessors()
+        );
+    }
+
+    public void emailTo(User user) {
+        pool.submit(
+                new Thread(
+                        () -> {
+                            send(subject(user), body(user), user.getEmail());
+                        }
+                )
+        );
+    }
+
+    public void close() {
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+
+    private String subject(User user) {
+        return "Notification  "
+                + user.getUsername()
+                + " to email "
+                + user.getEmail();
+    }
+
+    private String body(User user) {
+        return "Add a new event to " + user.getUsername();
+    }
+
+}
